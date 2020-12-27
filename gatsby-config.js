c33ee661394356d6e9f61933ede38e5b4023b538
RyanSamman <RyanSamman@outlook.com>
2020-12-27 19:22:18 +0300

    React SVGs

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,6 +70,14 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
