0b1ad893fb2fde3b52dd16aaec5e14464b7d217c
RyanSamman <RyanSamman@outlook.com>
2020-12-27 19:24:00 +0300

    Personallize Website

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,13 +60,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Ryan Samman's Blog`,
+        short_name: `Blog`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/icon.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
