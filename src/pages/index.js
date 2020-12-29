a16b99b33842d402b7d7f3e0335fdc91bbad39a5
RyanSamman <RyanSamman@outlook.com>
2020-12-29 22:39:46 +0300

    Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,77 +1,35 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
+import { Link } from "gatsby"
 import SEO from "../components/seo"
+import NavBar from "../components/NavBar"
 
-const BlogIndex = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata?.title || `Title`
-  const posts = data.allMarkdownRemark.nodes
+import TopWave from "./../../content/assets/topwave.svg"
+import BottomWave from "./../../content/assets/bottomwave.svg"
 
+const BlogIndex = () => {
   return (
     <>
-      <Layout location={location} title={siteTitle}>
-        <SEO title="All posts" />
-        <ol style={{ listStyle: `none` }}>
-          {posts.map(post => {
-            const title = post.frontmatter.title || post.fields.slug
+      <SEO title="Home" />
+      <TopWave />
+      <NavBar />
+      <div style={{ minHeight: "60vh", paddingTop: "10vh", color: "#FFF" }}>
+        <center>
+          <h1 className="text-white glow">
+            <Link to="/blog">Blog</Link>
+          </h1>
+          <h1 className="text-white glow">
+            <Link to="https://github.com/RyanSamman">Projects</Link>
+          </h1>
+          <h1 className="text-white glow">
+            <Link to="https://twitter.com/Ryan_Samman_">Contact</Link>
+          </h1>
+        </center>
+      </div>
 
-            return (
-              <li key={post.fields.slug}>
-                <article
-                  className="post-list-item"
-                  itemScope
-                  itemType="http://schema.org/Article"
-                >
-                  <header>
-                    <h2>
-                      <Link to={post.fields.slug} itemProp="url">
-                        <span itemProp="headline">{title}</span>
-                      </Link>
-                    </h2>
-                    <small>{post.frontmatter.date}</small>
-                  </header>
-                  <section>
-                    <p
-                      dangerouslySetInnerHTML={{
-                        __html: post.frontmatter.description || post.excerpt,
-                      }}
-                      itemProp="description"
-                    />
-                  </section>
-                </article>
-              </li>
-            )
-          })}
-        </ol>
-        <Bio />
-      </Layout>
+      <BottomWave height="300" />
     </>
   )
 }
 
 export default BlogIndex
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      nodes {
-        excerpt
-        fields {
-          slug
-        }
-        frontmatter {
-          date(formatString: "MMMM DD, YYYY")
-          title
-          description
-        }
-      }
-    }
-  }
-`
