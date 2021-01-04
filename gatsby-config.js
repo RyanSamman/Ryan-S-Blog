be0eb3437777148fdf8688ea5ec9bd3e29af3e70
RyanSamman <RyanSamman@outlook.com>
2021-01-05 00:39:22 +0300

    Fix Typo causing crashes

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,7 @@ module.exports = {
             },
           },
           {
-            resolve: `gatsby-remark-responsive-frame`,
+            resolve: `gatsby-remark-responsive-iframe`,
             options: {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
