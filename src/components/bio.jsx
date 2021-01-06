7751da037949e0a5e4de108ce6f7e55d110f0fb7
RyanSamman <RyanSamman@outlook.com>
2021-01-06 19:24:04 +0300

    Move Icons to bottom of bio

--- src/components/bio.jsx
+++ src/components/bio.jsx
@@ -43,31 +43,39 @@ const Bio = () => {
 
   return (
     <div className="md:flex bg-gray-100 justify-items-center rounded-xl p-8 md:p-0">
+      <div className="md:my-auto md:ml-8 content-center">
         <img
+          className="block w-32 h-32 rounded-full mx-auto"
           src="https://pbs.twimg.com/profile_images/1311819690528903168/GH9oF9U8_400x400.jpg"
           alt=""
           width="384"
           height="512"
         />
-          <a
-            href={`https://twitter.com/${social?.twitter || ``}`}
-          >
-            <TwitterIcon />
-          </a>
-          <a
-            href={`https://github.com/${social?.github || ``}`}
-          >
-            <GitHubIcon />
-          </a>
-        </div>
       </div>
+      <div className="pt-4 md:p-8 text-center md:text-left space-y-4">
+        <p className="text-lg font-semibold mb-4">
           Currently a freshman student in FCIT, I created this blog to document
           my programming journey and the projects I create. My strengths are in
           Python and Javascript, however, I plan on diving deeper into Computer
           Science in the coming years.
         </p>
         <figcaption className="font-medium">
+          <div className="text-blue-800">Ryan Samman</div>
           <div className="text-gray-500">Computer Science Student, FCIT</div>
+          <div style={{ width: "fit-content" }} className="mx-auto md:mx-0 mt-2">
+            <a
+              href={`https://twitter.com/${social?.twitter || ``}`}
+              className="mr-1"
+            >
+              <TwitterIcon />
+            </a>
+            <a
+              href={`https://github.com/${social?.github || ``}`}
+              className="mr-1"
+            >
+              <GitHubIcon />
+            </a>
+          </div>
         </figcaption>
       </div>
     </div>
