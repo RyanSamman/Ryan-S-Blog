6b5712da29a47aafb85830a6d776375c3fe879ca
RyanSamman <RyanSamman@outlook.com>
2021-01-06 21:23:04 +0300

    Prompt user when webpage is stale

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,14 @@
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+
+  if (answer === true) {
+    window.location.reload()
+  }
+}
+
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
