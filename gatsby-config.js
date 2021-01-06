5ac663d78aa12a24f97232de784ce6488f23401f
RyanSamman <RyanSamman@outlook.com>
2021-01-06 20:28:30 +0300

    Fix Maskable icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,42 +75,47 @@ module.exports = {
         // icon: `content/assets/icon.png`,
         icons: [
           {
-            src: "static/images/icons/icon-72x72.png",
+            src: "static/images/icons/maskable_icon_x72",
             sizes: "72x72",
             type: "image/png",
           },
           {
-            src: "static/images/icons/icon-96x96.png",
+            src: "static/images/icons/maskable_icon_x96",
             sizes: "96x96",
             type: "image/png",
           },
           {
-            src: "static/images/icons/icon-128x128.png",
+            src: "static/images/icons/maskable_icon_x128.png",
             sizes: "128x128",
             type: "image/png",
           },
           {
-            src: "static/images/icons/icon-144x144.png",
+            src: "static/images/icons/maskable_icon_x144.png",
             sizes: "144x144",
             type: "image/png",
           },
           {
-            src: "static/images/icons/icon-152x152.png",
+            src: "static/images/icons/maskable_icon_x152.png",
             sizes: "152x152",
             type: "image/png",
           },
+          // {
+          //   src: "static/images/icons/icon-192x192.png",
+          //   sizes: "192x192",
+          //   type: "image/png",
+          // },
           {
-            src: "static/images/icons/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "static/images/icons/icon-384x384.png",
+            src: "static/images/icons/maskable_icon_x384.png",
             sizes: "384x384",
             type: "image/png",
           },
           {
-            src: "static/images/icons/icon-512x512.png",
+            src: "static/images/icons/maskable_icon_x480.png",
+            sizes: "480x480",
+            type: "image/png",
+          },
+          {
+            src: "static/images/icons/maskable_icon_x152.png",
             sizes: "512x512",
             type: "image/png",
           },
