8059e4f5537ce19ec839b5abd11a869807d622b1
RyanSamman <RyanSamman@outlook.com>
2021-01-06 16:58:30 +0300

    Rename .js files to .jsx

--- src/components/bio.jsx
+++ src/components/bio.jsx
@@ -43,38 +43,30 @@ const Bio = () => {
 
   return (
     <div className="md:flex bg-gray-100 justify-items-center rounded-xl p-8 md:p-0">
-      <div className="md:my-auto">
         <img
-          className="block w-32 h-32 rounded-full mx-auto md:mx-2"
           src="https://pbs.twimg.com/profile_images/1311819690528903168/GH9oF9U8_400x400.jpg"
           alt=""
           width="384"
           height="512"
         />
-        <div className="flex justify-center mt-4">
           <a
             href={`https://twitter.com/${social?.twitter || ``}`}
-            className="mr-2"
           >
             <TwitterIcon />
           </a>
           <a
             href={`https://github.com/${social?.github || ``}`}
-            className="ml-2"
           >
             <GitHubIcon />
           </a>
         </div>
       </div>
-      <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
-        <p className="text-lg font-semibold">
           Currently a freshman student in FCIT, I created this blog to document
           my programming journey and the projects I create. My strengths are in
           Python and Javascript, however, I plan on diving deeper into Computer
           Science in the coming years.
         </p>
         <figcaption className="font-medium">
-          <div className="text-cyan-600">Ryan Samman</div>
           <div className="text-gray-500">Computer Science Student, FCIT</div>
         </figcaption>
       </div>
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import NavBar from "./../components/NavBar"
+import NavBar from "./NavBar"
 
 import TopWave from "./../../content/assets/topwave.svg"
 import BottomWave from "./../../content/assets/bottomwave.svg"
